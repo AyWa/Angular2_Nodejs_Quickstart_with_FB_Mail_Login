ab98a1188e263d89260d8110ccad164c28024ea9
HMarc <marc.hurabielle@gmail.com>
2016-10-09 16:25:20 +0200

    correct passeport.js directory

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var fbStrategy = require('passport-facebook').Strategy;
 // load up the user model
-var User = require('../app/models/user');
+var User = require('../models/user');
 var config = require('../config/database'); // get db config file
 
 module.exports = function(passport) {
